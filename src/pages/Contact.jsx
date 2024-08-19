a9e1c5de4421b29408e601b8b05ef5ac066809c6
stanimeros <pantelisstanimeros@outlook.com>
2024-08-20 01:30:43 +0300

    added alerts

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,19 +1,83 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from "react-router-dom";
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 import Translator from '../widgets/Translator';
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
 
 function Contact({GlobalState}) {
   const {
     api, language,
   } = GlobalState;
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    alert("Form submitted!");
-  }
+  const formRef = useRef(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault(); // Prevent the default form submission
+
+    // Collect form data by IDs
+    const form = formRef.current;
+    const name = document.getElementById('name').value;
+    const service = document.getElementById('service').value;
+    const email = document.getElementById('email').value;
+    const terms = document.getElementById('terms').checked;
+
+    // Validate the form data
+    if (!email || !terms) {
+      Toastify({
+        text: Translator({code:language, value:"Please fill the email field and accept the terms."}).props.children,
+        duration: 3000,
+        className: "toast warning",
+        gravity: "bottom",
+        position: "right",
+      }).showToast();
+      return;
+    }
+
+    const formData = {
+      name: name,
+      service: service,
+      email: email,
+      terms: terms ? 'Accepted' : 'Not accepted',
+    };
+
+    // Send the form data to the PHP script
+    try {
+      const response = await fetch(`/api/send_email.php`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+      if (data.status=="success"){
+        form.reset();
+
+        Toastify({
+          text: Translator({code:language, value:"Success"}).props.children,
+          duration: 3000,
+          className: "toast success",
+          gravity: "bottom",
+          position: "right",
+        }).showToast();
+      }else{
+        throw new Error("Data Status Error");
+      }
+    } catch (error) {
+      console.error('Contact Form:', error);
+      Toastify({
+        text: Translator({code:language, value:"Oops! Something didn't go as planned"}).props.children,
+        duration: 3000,
+        className: "toast warning",
+        gravity: "bottom",
+        position: "right",
+      }).showToast();
+    }
+  };
 
   return (
     <>
@@ -26,10 +90,7 @@ function Contact({GlobalState}) {
       <main>
         <div>/Get In Touch</div>
         <h1>Schedule an Appointment</h1>
-        <form
-          className='contact-form' 
-          onSubmit={handleSubmit}
-        >
+        <form ref={formRef} className='contact-form' onSubmit={handleSubmit}>
           <div className='main-form'>
             <div className='row'>
               <div>Hey my name is</div>
@@ -43,9 +104,9 @@ function Contact({GlobalState}) {
               </input>
               <div>and I am looking for</div>
               <select 
+                id='service'
+                name='service'
                 className='input dropdown'
-                id='services'
-                name='services'
                 placeholder='Select Dropdown'
               >
                 <option value='' disabled selected>Select Dropdown</option>
@@ -61,12 +122,11 @@ function Contact({GlobalState}) {
             <div className='row'>
               <div>Get in touch with me at</div>
               <input 
-                className='input' 
                 id='email'
                 name='email'
                 type='email'
+                className='input' 
                 placeholder='Your Email Here'
-                required
               >
               </input>
               <div>!</div>
@@ -74,7 +134,7 @@ function Contact({GlobalState}) {
           </div>
           <div class="row checkbox-wrapper-65">
             <label for="terms">
-              <input type="checkbox" id="terms" required></input>
+              <input id="terms" type="checkbox"></input>
               <span class="cbx">
                 <svg width="12px" height="11px" viewBox="0 0 12 11">
                   <polyline points="1 6.29411765 4.5 10 11 1"></polyline>
